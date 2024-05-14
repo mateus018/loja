d54e41480bd62f2aa1a10188ade81382596db230
Mateus do Nascimento Rodrigues <mateus.nascimento018@gmail.com>
2024-05-13 21:35:38 -0300

    modal and cart

--- script.js
+++ script.js
@@ -1,83 +1,206 @@
+let cart =[];
+let modalKey = 0;
+let modalQt = 1;
+const c = (el) =>document.querySelector(el);
+const cs = (el)=>document.querySelectorAll(el);
+
 
 
 
 const url = "https://fakestoreapi.com/products"
 
 
-async function get() {
+async function get(){
+
+    const response = await fetch(url);
+    let data = await response.json();
+
+    data.map((item, index)=>{
+        
+        let productItem = c('.models .product-item').cloneNode(true);
+        
+
+        productItem.setAttribute('data-key',index);
+ 
+        productItem.querySelector('.product--img img').src = item.image;
+        productItem.querySelector('.product-title').innerHTML = item.title;
+        productItem.querySelector('.product-price').innerHTML = `R$ ${item.price.toFixed(2)}`
+        productItem.querySelector('.product-item .addTocart').addEventListener('click',(e) => {
+
+            e.preventDefault();
+            let key =  e.target.closest('.product-item').getAttribute('data-key');
+            modalKey = key;
+
+            c('.productBig img').src = data[key].image;
+            c('.productInfo h1').innerHTML = data[key].title;
+            c('.productInfo--desc').innerHTML = data[key].description;
+            c('.productInfo--actualPrice').innerHTML = `R$ ${data[key].price.toFixed(2)}`
+
+
+
+            c('.productWindowArea').style.opacity = 0;
+            c('.productWindowArea').style.display = 'flex';
+            setTimeout(()=>{
+                c('.productWindowArea').style.opacity = 1;  
+            },250)
+
+        });
+
+        
+       
+
+
+
+        c('.products-area').append(productItem);
+
+
+        
+ 
+    });
+
+
+    c('.productInfo--addButton').addEventListener('click',()=>{
+        
+        let identifier = data[modalKey].id;
+        let key = cart.findIndex((item)=>item.identifier==identifier);
+        console.log(key);
+        if(key>-1){
+            cart[key].qt += modalQt;
+        } else {
+            cart.push({
+                identifier,
+                id: data[modalKey].id,
+                qt:modalQt
+            });
+        }
+        
+        
+        
+        
+        updateCart();
+        closeModal();
+    });
+
+    c('.menu-openner').addEventListener('click',()=>{
+
+        if(cart.length>0){
+            c('aside').style.left = '0';
+        }
     
-    const response = await fetch(url)
-
-    console.log(response);
-
-    const data =await response.json();
-    console.log(data);
-
-    data.map((item)=>{
-
-        const mainContent = document.getElementById("main-content");
-        
-        const container =  document.createElement("div");
-        container.className = "container"
-        
-        const product = document.createElement("div");
-        product.className = "product"
-        
-        const image = document.createElement("div");
-        image.className = "image"
-
-        const productImage = document.createElement("img");
-        productImage.className = "product-image"
-        productImage.src=item.image;
-
-        const title = document.createElement("div");
-        title.className = "title"
-        title.innerText = item.title;
-
-        const detail = document.createElement("div");
-        detail.className = "detail"
-
-        const price = document.createElement("p");
-        price.innerText = "$"
-        
-
-        const valor = document.createElement("span");
-        valor.innerText = item.price.toFixed(2);
-        
-
-        const botaoCart = document.createElement("a");
-        botaoCart.innerText = "add to cart"
-        botaoCart.href = "#"
-
-        price.appendChild(valor)
-        
-        detail.appendChild(price)
-        detail.appendChild(botaoCart)
-
-        image.appendChild(productImage)
-
-        product.appendChild(image)
-        product.appendChild(title)
-        product.appendChild(detail)
-        
-        container.appendChild(product)
-
-        mainContent.appendChild(container)
-
-        
-        
-
-
-
-
-        
     
-    
+    });
+
+    c('.menu-closer').addEventListener('click',()=>{
+        c('aside').style.left = '100vw';
     })
+
+    function updateCart(){
+
+        
+        c('.menu-openner span').innerHTML = cart.length;
+        if(cart.length>0){
+            c('aside').style.display='block'
+            c('aside').classList.add('show');
+            c('.cart').innerHTML = '';
+            
+            let subtotal = 0;
+            let desconto = 0;
+            let total = 0;
+    
+            for(let i in cart){
+                let productItem = data.find((item)=>item.id == cart[i].id);
+                subtotal += productItem.price*cart[i].qt;
+                
+                
+                let cartItem = c('.models .cart--item').cloneNode(true);
+               
+                
+    
+                
+    
+    
+                cartItem.querySelector('img').src = productItem.image;
+                cartItem.querySelector('.cart--item-nome').innerHTML = productItem.title;
+    
+                cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
+                cartItem.querySelector('.cart--item-qtmenos').addEventListener('click',()=>{
+                    if(cart[i].qt>1){
+                        cart[i].qt--;
+                    } else{
+                        cart.splice(i,1);
+                       
+                    }
+    
+                    updateCart();
+                })
+                cartItem.querySelector('.cart--item-qtmais').addEventListener('click',()=>{
+                    cart[i].qt++;
+                    updateCart();
+                })
+    
+                c('.cart').append(cartItem);
+            }
+            desconto = subtotal*0.1;
+            total = subtotal- desconto;
+    
+            c('.subtotal span:last-child').innerHTML =  `R$ ${subtotal.toFixed(2)}`;
+            c('.desconto span:last-child').innerHTML =  `R$ ${desconto.toFixed(2)}`;
+            c('.total span:last-child').innerHTML =  `R$ ${total.toFixed(2)}`;
+        } else{
+            c('aside').classList.remove('show');
+            c('aside').style.display = 'none'
+            
+        }
+    
+    
+    }
 
 }
 
+function closeModal() {
+    c('.productWindowArea').style.opacity = 0;
+    setTimeout(()=>{
+        c('.productWindowArea').style.display = 'none'  
+    },500);
+
+}
+
+cs('.productInfo--cancelButton, .productInfo--cancelMobileButton').forEach((item)=>{
+    item.addEventListener('click', closeModal);
+});
+
+c('.productInfo--qtmenos').addEventListener('click',()=>{
+    
+    if(modalQt>1){
+        modalQt--;
+        c('.productInfo--qt').innerHTML = modalQt;
+    }
+    
+});
+
+c('.productInfo--qtmais').addEventListener('click',()=>{
+    
+    modalQt++;
+    c('.productInfo--qt').innerHTML = modalQt;
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 get();
+
+
+
 
 
